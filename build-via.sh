a807f3466a365e47b6940a21cae6bd474b2b63e8
Yimura <24669514+Yimura@users.noreply.github.com>
2025-08-02 11:44:07 +0200

    fix: device not detected because of late custom device definition loading

--- build-via.sh
+++ build-via.sh
@@ -2,6 +2,8 @@ git clone git@github.com:the-via/app via-app
 cd via-app
 npm install
 sed -i 's|^\(\s*\)//\(.*fetch.*\)|\1\2|; s|^\(\s*\)\(const hash = document.getElementById.*\)|\1//\2|' src/utils/device-store.ts
+# Fixes permanent loading issue
+sed -i 's/dispatch(loadStoredCustomDefinitions/await dispatch(loadStoredCustomDefinitions/' src/store/devicesThunks.ts
 npm run build
 
 cp -R dist/ ../public
