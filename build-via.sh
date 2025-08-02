27729be42086ccb65b6fc3f788885806a6af8335
Andreas Maerten <24669514+Yimura@users.noreply.github.com>
2025-08-02 21:57:13 +0200

    fix: device not detected because of late custom device definition loading (#16)
    This fixes an issue (at the least for people with custom device
    definitions), where their keyboard would not be detected before the via
    application reloads its connected devices.
    
    Potential fix for #13
    
    ---------
    
    Co-authored-by: Cebby <cebby2420@outlook.com>

--- build-via.sh
+++ build-via.sh
@@ -5,6 +5,8 @@ cd "$VIA_DIR"
 bun i
 bun run refresh-kbs
 sed -i 's|^\(\s*\)//\(.*fetch.*\)|\1\2|; s|^\(\s*\)\(const hash = document.getElementById.*\)|\1//\2|' src/utils/device-store.ts
+# Fixes permanent loading issue - https://github.com/cebby2420/via-desktop/issues/13#issuecomment-3146391508
+sed -i 's/dispatch(loadStoredCustomDefinitions/await dispatch(loadStoredCustomDefinitions/' src/store/devicesThunks.ts
 bun run build
 
 cp -R dist/ ../public
